60f50ba160c7651298718c5bec18f662d3b05cf2
mulls528 <mulls528@newschool.edu>
2024-04-16 11:08:29 -0400

    again

--- studio/browser-extension/script.js
+++ studio/browser-extension/script.js
@@ -1,3 +1,7 @@
 let allText = document.querySelectorAll("*");
 
-console.log("altText")
+//creating "for of" loop to target allText//
+for(let text of allText) {
+//add new style and name classlist "font-change"//
+text.classList.add("font-change");
+}
