319ce2138e2fd286aa6d20ed160c4114dd49c11c
mulls528 <mulls528@newschool.edu>
2024-04-16 16:45:03 -0400

    lab hw 1

--- lab/week-12/script.js
+++ lab/week-12/script.js
@@ -1,7 +1,28 @@
-var apiUrl = "https://api.weather.gov/points/40.730610,-73.935242";
+fetch("https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json")
 
-fetch(apiUrl)
-  .then(response => response.json())
+.then(response => response.json())
   .then(data => {
-    var apiUrl = data;
+    var allSchools = data;
+
+//display only schools in France//
+var schoolsInFrance = allSchools.filter(school => school.country === 'France');
+schoolsInFrance.sort((a, b) => a.name.localeCompare(b.name));
+
+schoolsInFrance.forEach(school => {
+  var element = document.createElement('div');
+
+    // allSchools.sort((a, b) => a.name.localeCompare(b.name));
+  
+    // allSchools.forEach(school => {
+    //   var element = document.createElement('div');
+     
+      element.className = 'school-card';
+      element.innerHTML = `
+        <h2>${school.name}</h2>
+        <p>Website: <a href="${school.web_pages[0]}" target="_blank">${school.web_pages[0]}</a></p>
+        <p>Domain: ${school.domains}</p>
+      `;
+      
+      document.body.appendChild(element);
+    });
   });
--- studio/browser-extension/script.js
+++ studio/browser-extension/script.js
@@ -1,7 +1,14 @@
-let allText = document.querySelectorAll("*");
+// let allText = document.querySelectorAll("*");
 
-//creating "for of" loop to target allText//
-for(let text of allText) {
-//add new style and name classlist "font-change"//
-text.classList.add("font-change");
+// //creating "for of" loop to target allText//
+// for(let text of allText) {
+// //add new style and name classlist "font-change"//
+// text.classList.add("font-change");
+// }//
+
+let allImages = document.querySelectorAll("*");
+
+for(let thing of allImages) {
+    thing.src = "https://www.secondcity.com/wp-content/uploads/2014/09/SC_Alumni_Murray_Bill_600x600_001.jpg";
+    thing.srcset = "https://www.secondcity.com/wp-content/uploads/2014/09/SC_Alumni_Murray_Bill_600x600_001.jpg";
 }
